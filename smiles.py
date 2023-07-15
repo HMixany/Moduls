f37f8282b3264b870a5e76fabf35917814d39c6d
HMixany <137229468+HMixany@users.noreply.github.com>
2023-07-15 23:03:25 +0300

    six commit

---
+++ smiles.py
@@ -0,0 +1,14 @@
+print("\u263A")
+print("\U0001F600")
+
+name = "Alice"
+age = 30
+message = "My name is {}. I am {} years old.".format(name, age)
+print(message)
+message = f"My name is {name}. I am {age} years old"
+print(message)
+message = "My name is " + name + ". I am " + str(age) + " years old."
+print(message)
+message = f"My name is {name}. I am {age} years old."
+print(message)
+
